9a8354d74f6c03070fff0339ef98221fb9914816
9hannahnine-jpg <9hannahnine@gmail.com>
2026-04-27 15:59:04 -0500

    Bump to 3.2.4

--- arc_sentry/__init__.py
+++ arc_sentry/__init__.py
@@ -2,5 +2,5 @@ from arc_sentry.core.pipeline import ArcSentryV3, ArcSentryV2
 from arc_sentry.models.mistral_adapter import MistralAdapter
 from arc_sentry.models.qwen_adapter import QwenAdapter
 
-__version__ = "3.2.3"
+__version__ = "3.2.4"
 __all__ = ["ArcSentryV3", "ArcSentryV2", "MistralAdapter", "QwenAdapter"]
